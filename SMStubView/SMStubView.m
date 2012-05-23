4eab854948c192716f7f06c6f5898d4f483ab83d
Hanief Cahya Utama <hanief@gmail.com>
2012-05-23 13:44:50 +0700

    customized for bango version 1

--- SMStubView/SMStubView.m
+++ SMStubView/SMStubView.m
@@ -16,33 +16,30 @@
 	self = [super initWithFrame:frame];
 	if (self) {
 		self.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"stubview.png"]];
-		_scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(self.center.x-(self.bounds.size.width/2/2), self.bounds.origin.y, (self.bounds.size.width/2), self.bounds.size.height)];
+        _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(self.center.x-(self.bounds.size.width/2/2/2), self.bounds.origin.y, (self.bounds.size.width/2/2), self.bounds.size.height)];
         _scrollView.delegate = self;
 		_scrollView.clipsToBounds = NO;
 		_scrollView.pagingEnabled = YES;
 		_scrollView.showsVerticalScrollIndicator = NO;
 		_scrollView.showsHorizontalScrollIndicator = NO;
-		_scrollView.contentSize = CGSizeMake(self.bounds.size.width, self.bounds.size.height*2);
-		[self addSubview:_scrollView];
-		_leftView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"left.png"]];
-		_leftView.frame = CGRectMake(self.bounds.origin.x, self.bounds.origin.y, _leftView.frame.size.width, _leftView.frame.size.height);
-		[self addSubview:_leftView];
-		_leftView.hidden = YES;
-		_rightView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"right.png"]];
-		_rightView.frame = CGRectMake(self.bounds.size.width-(_rightView.frame.size.width), self.bounds.origin.y, _leftView.frame.size.width, _leftView.frame.size.height);
-		[self addSubview:_rightView];
-		
+		_scrollView.contentSize = CGSizeMake(self.bounds.size.width, self.bounds.size.height);
+        _scrollView.canCancelContentTouches = YES;
+		//[self addSubview:_scrollView];
+        
 		_isUsingScrollView = YES;
 	}
 	return self;
 }
 
+
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
 	if ([self pointInside:point withEvent:event]) {
-		return _scrollView;
-	}
+        //NSLog(@"hit");
+        return _scrollView;
+    }
 	return nil;
 }
+
 
 - (void)setMenus:(NSArray *)menus {
 	[_menus release];
@@ -50,29 +47,85 @@
 	CGFloat contentOffset = 0;
 	for (NSUInteger i=0; i<_menus.count; i++) {
 		NSString *menu = [_menus objectAtIndex:i];
-		CGRect labelFrame = CGRectMake(contentOffset, 0, 160, self.bounds.size.height);
+		CGRect labelFrame = CGRectMake(contentOffset, 0, 160/2, self.bounds.size.height);
+        CGSize labelsize;
 		UILabel *label = [[UILabel alloc] initWithFrame:labelFrame];
+        [label setNumberOfLines:0];
 		label.backgroundColor = [UIColor clearColor];
 		label.textColor = [UIColor whiteColor];
 		label.textAlignment = UITextAlignmentCenter;
-		label.font = [UIFont systemFontOfSize:16];
-		label.text = menu;
+		label.font = [UIFont systemFontOfSize:12];
+		label.text = [menu uppercaseString];
+        labelsize = [menu sizeWithFont:label.font constrainedToSize:CGSizeMake(80,44) lineBreakMode:UILineBreakModeWordWrap];
+        label.frame = CGRectMake(contentOffset+2, 0, 160/2, 44);
+        [label setTag:i+1];
+        //[label setUserInteractionEnabled:YES];
 		[_scrollView addSubview:label];
-		[label release];
-		contentOffset += 160;
-		_scrollView.contentSize = CGSizeMake(160*(i+1), 23);
+		
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.frame = label.frame;
+        [button addTarget:self action:@selector(labelButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+        [button setBackgroundColor:[UIColor clearColor]];
+        [button setTag:i+11];
+        [button setUserInteractionEnabled: YES];
+        [label addSubview:button];
+        
+        if (i!=0) {
+            UIImageView *border = [[UIImageView alloc] initWithFrame:CGRectMake(label.frame.origin.x-2, 0, 2, 44)];
+            border.image = [UIImage imageNamed:@"line.png"];
+            [_scrollView addSubview:border];
+            [border release];
+        }
+        
+		contentOffset += 82;
+		_scrollView.contentSize = CGSizeMake(82*(i+1), 44);
+        [label release];
 	}
+    [self addSubview:_scrollView];
+}
+
+- (void)labelButtonPressed:(id)sender{
+    NSLog(@"button pressed");
+}
+
+- (int)activeIndex{
+    CGFloat pageWidth = _scrollView.frame.size.width;
+	int index = floor((_scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+    return index;
 }
 
 - (void)scrollToIndex:(int)index {
+    NSLog(@"index: %d",index);
 	CGRect frame;
-	frame.origin.x = _scrollView.frame.size.width * index;
+	frame.origin.x = 82*index;
 	frame.origin.y = 0;
 	frame.size = _scrollView.frame.size;
 	[_scrollView scrollRectToVisible:frame animated:YES];
 	_isUsingScrollView = NO;
 	[_scrollView scrollRectToVisible:frame animated:YES];
-	
+    
+    UILabel *label = [_scrollView viewWithTag:index+1];
+    [label setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"stubview-press.png"]]];
+    [label setTextColor:[UIColor colorWithRed:00.0/255.0 green:133.0/255.0 blue:126.0/255.0 alpha:1.0]];
+    [label setFont:[UIFont boldSystemFontOfSize:12]];
+    
+    if (index!=0) {
+        UILabel *prevLabel = [_scrollView viewWithTag:index];
+        [prevLabel setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"stubview.png"]]];
+        [prevLabel setTextColor:[UIColor whiteColor]];
+        [prevLabel setFont:[UIFont systemFontOfSize:12]];
+    }
+    
+    if (index<_menus.count-1) {
+        UILabel *nextLabel = [_scrollView viewWithTag:index+2];
+        [nextLabel setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"stubview.png"]]];
+        [nextLabel setTextColor:[UIColor whiteColor]];
+        [nextLabel setFont:[UIFont systemFontOfSize:12]];
+    }
+    
+    if ([_delegate respondsToSelector:@selector(stubViewDidEndDeceleratingToIndex:)]) {
+		[_delegate stubViewDidEndDeceleratingToIndex:index];
+	}
 }
 
 #pragma mark -
@@ -80,15 +133,38 @@
 
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
 	_isUsingScrollView = YES;
+   // NSLog(@"begin drag");
 }
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
 	_isUsingScrollView = YES;
+    
 	CGFloat pageWidth = _scrollView.frame.size.width;
 	int index = floor((_scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+    
+    UILabel *label = [_scrollView viewWithTag:index+1];
+    [label setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"stubview-press.png"]]];
+    [label setTextColor:[UIColor colorWithRed:00.0/255.0 green:133.0/255.0 blue:126.0/255.0 alpha:1.0]];
+    [label setFont:[UIFont boldSystemFontOfSize:12]];
+    
+    if (index!=0) {
+        UILabel *prevLabel = [_scrollView viewWithTag:index];
+        [prevLabel setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"stubview.png"]]];
+        [prevLabel setTextColor:[UIColor whiteColor]];
+        [prevLabel setFont:[UIFont systemFontOfSize:12]];
+    }
+    
+    if (index<_menus.count-1) {
+        UILabel *nextLabel = [_scrollView viewWithTag:index+2];
+        [nextLabel setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"stubview.png"]]];
+        [nextLabel setTextColor:[UIColor whiteColor]];
+        [nextLabel setFont:[UIFont systemFontOfSize:12]];
+    }
+    
 	if ([_delegate respondsToSelector:@selector(stubViewDidEndDeceleratingToIndex:)]) {
 		[_delegate stubViewDidEndDeceleratingToIndex:index];
 	}
+    
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
